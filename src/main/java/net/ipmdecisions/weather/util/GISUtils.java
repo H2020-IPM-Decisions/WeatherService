d4835bf6ccf87cc5631cc1dc81aab9d5c4ee7002
Tor-Einar Skog <tor-einar.skog@nibio.no>
2021-11-23 10:48:15 +0100

    Now spatial info for country is also used when searching for a suitable data source

--- src/main/java/net/ipmdecisions/weather/util/GISUtils.java
+++ src/main/java/net/ipmdecisions/weather/util/GISUtils.java
@@ -19,11 +19,25 @@
 
 package net.ipmdecisions.weather.util;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.wololo.geojson.Feature;
+import org.wololo.geojson.FeatureCollection;
+import org.wololo.geojson.GeoJSONFactory;
+import org.wololo.jts2geojson.GeoJSONWriter;
+
 /**
- * @copyright 2020 <a href="http://www.nibio.no/">NIBIO</a>
+ * @copyright 2021 <a href="http://www.nibio.no/">NIBIO</a>
  * @author Tor-Einar Skog <tor-einar.skog@nibio.no>
  */
 public class GISUtils {
+	
+	private FeatureCollection allCountryBoundaries = null;
 
     /**
      * Ref this post: https://gis.stackexchange.com/questions/14449/java-vividsolutions-jts-wgs-84-distance-to-meters
@@ -33,5 +47,53 @@ public class GISUtils {
     public Double getDistanceInMetersWGS84(Double jtsDistanceAngularUnits)
     {
         return jtsDistanceAngularUnits * (Math.PI/180) * 6378137;
+    }
+    
+    public FeatureCollection getCountryBoundaries(){
+        if(this.allCountryBoundaries == null)
+        {
+            try {
+                FileReader in = new FileReader(System.getProperty("net.ipmdecisions.weatherservice.COUNTRY_BOUNDARIES_FILE"));
+                BufferedReader br = new BufferedReader(in);
+                String all = "";
+                String line;
+                while((line = br.readLine()) != null)
+                {
+                    all += line;
+                }
+
+                this.allCountryBoundaries = (FeatureCollection) GeoJSONFactory.create(all);
+            } catch (IOException | NullPointerException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return this.allCountryBoundaries;
+    }
+    
+    public Feature getCountryBoundary(String countryCode)
+    {
+        FeatureCollection countryBoundaries = this.getCountryBoundaries();
+        for(Feature feature:countryBoundaries.getFeatures())
+        {
+            if(feature.getProperties().get("ISO_A3").equals(countryCode))
+            {
+                return feature;
+            }
+        }
+        return null;
+    }
+    
+    public FeatureCollection getCountryBoundaries(Set<String> countryCodes)
+    {
+        List<Feature> matching = new ArrayList<>();
+        FeatureCollection countryBoundaries = this.getCountryBoundaries();
+        for(Feature feature:countryBoundaries.getFeatures())
+        {
+            if(countryCodes.contains((String) feature.getProperties().get("ISO_A3")))
+            {
+                matching.add(feature);
+            }
+        }
+        return new GeoJSONWriter().write(matching);
     }
 }
