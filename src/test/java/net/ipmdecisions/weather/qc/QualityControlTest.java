e921d10afae60397c578723f292e4528bcf86d15
Aleksi Toivanen <aleksi.toivanen@luke.fi>
2021-12-07 23:07:52 +0200

    test(qc): add smaller unit tests
    Changes:
    
    * Add new unit tests to test qc functionality in smaller sizes. This should make it easier to see where quality checks are broken.
    * Some of the new tests are still failing in places where the implemented functionality was incorrect. Fixes will be implemented in a future commit.

--- src/test/java/net/ipmdecisions/weather/qc/QualityControlTest.java
+++ src/test/java/net/ipmdecisions/weather/qc/QualityControlTest.java
@@ -7,8 +7,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Arrays;
 import java.util.List;
 import net.ipmdecisions.weather.entity.LocationWeatherData;
+import net.ipmdecisions.weather.entity.QCType;
 
 //import static org.junit.jupiter.api.Assertions.*;
 
@@ -46,6 +48,10 @@ public class QualityControlTest {
     public void tearDown() {
     }
     
+    // ------------------------------------------------
+    // COMPLEX OVERALL UNIT TESTS
+    // ------------------------------------------------
+
     @Test
     public void testRTQualityControl() throws Exception{
         
@@ -64,7 +70,6 @@ public class QualityControlTest {
         
         assertArrayEquals(expResult,result);
     }
-    
     
     @Test
     public void testRTQualityControlFail() throws Exception{
@@ -122,7 +127,511 @@ public class QualityControlTest {
         
         assertNotEquals(expResult,result);
     }
-    
-    
-}
 
+    // ------------------------------------------------
+    // HELPER FUNCTIONS
+    // ------------------------------------------------
+
+    /**
+     * Helper function for creating a new instance of `WeatherData` with
+     * given data and weather parameters inside it.
+     * 
+     * @param weatherParameters - weather parameters to insert inside `WeatherData`.
+     * @param data - data to insert inside `WeatherData`.
+     * @return `WeatherData` instance containing given weather parameters and data.
+     * @throws Exception 
+     */
+    private static WeatherData getWeatherDataForTests(Integer[] weatherParameters, Double[][] data) throws Exception {
+        Integer[] qc = new Integer[weatherParameters.length];
+        Arrays.fill(qc, QCType.NO_QC);
+                
+        FileUtils fileUtils = new FileUtils();
+    	String weatherDataJson = fileUtils.getStringFromFileInApp("/weatherdata_empty.json");
+        ObjectMapper oMapper = new ObjectMapper();
+        WeatherData testData = oMapper.readValue(weatherDataJson, WeatherData.class);
+        
+        List<LocationWeatherData> locationWeatherData = testData.getLocationWeatherData();
+        locationWeatherData.get(0).setData(data);
+        locationWeatherData.get(0).setQC(qc);
+
+        testData.setWeatherParameters(weatherParameters);
+        testData.setLocationWeatherData(locationWeatherData);
+
+        return testData;
+    }
+    
+    /**
+     * Helper function for getting QC results array (for weather parameters)
+     * given WeatherData and a QC type.
+     * 
+     * @param testData - weather data
+     * @param QCType - string: either "RT" or "NONRT".
+     * @return QC results for weather parameters.
+     * @throws Exception 
+     */
+    private static Integer[] getQCResultForTests(WeatherData testData, String QCType) throws Exception {
+        QualityControlMethods instance = new QualityControlMethods();
+        WeatherData wd = instance.getQC(testData, QCType);
+        List<LocationWeatherData> lwd = wd.getLocationWeatherData();
+        Integer[] result = lwd.get(0).getQC();
+        
+        return result;
+    }
+    
+    /**
+     * Prints the name of the method (that called this method) on a new line.
+     * 
+     * As it is used inside a unit test, it prints out the name of the unit test.
+     */
+    private static void printTestName() {
+        System.out.println(new Exception().getStackTrace()[1].getMethodName());
+    }
+    
+    // ------------------------------------------------
+    // SMALLER FEATURE BASED UNIT TESTS
+    // ------------------------------------------------
+    
+    // ------------------------
+    // RT - PREQUALIFICATION QC
+    // ------------------------
+
+    @Test
+    public void testRTQCPrequalificationSuccess() throws Exception {
+
+        QualityControlTest.printTestName();
+
+        Integer[] weatherParameters = {1001};
+        Double[][] data = {{2.0}, {0.0}};
+        
+        WeatherData testData = QualityControlTest.getWeatherDataForTests(weatherParameters, data);
+        Integer[] result = QualityControlTest.getQCResultForTests(testData, "RT");
+
+        Integer[] expResult = {2};
+
+        assertArrayEquals(expResult,result);
+    }
+    
+    @Test
+    public void testRTQCPrequalificationNull() throws Exception {
+
+        QualityControlTest.printTestName();
+
+        Integer[] weatherParameters = {1001};
+        Double[][] data = {{null}, {0.0}};
+        
+        WeatherData testData = QualityControlTest.getWeatherDataForTests(weatherParameters, data);
+        Integer[] result = QualityControlTest.getQCResultForTests(testData, "RT");
+
+        Integer[] expResult = {4};
+
+        assertArrayEquals(expResult,result);
+    }
+
+    /**
+     * Check that strings in data are marked to fail prequalification QC for
+     * real time data.
+     * 
+     * @todo Both the test and the check for strings need to be done differently.
+     * @throws Exception 
+     */
+    @Test
+    public void testRTQCPrequalificationString() throws Exception {
+        QualityControlTest.printTestName();
+
+        Integer[] weatherParameters = {1001};
+        // TODO: both test and checking for strings needs to be done differently.
+        Double[][] data = {{"a string is not a double"}, {0.0}};
+        
+        WeatherData testData = QualityControlTest.getWeatherDataForTests(weatherParameters, data);
+        Integer[] result = QualityControlTest.getQCResultForTests(testData, "RT");
+
+        Integer[] expResult = {4};
+
+        assertArrayEquals(expResult,result);
+    }
+
+    // ------------------------
+    // RT - INTERVAL QC
+    // ------------------------
+
+    @Test
+    public void testRTQCIntervalSuccess() throws Exception {
+        /*
+        "parameter":"Air temperature",
+        "id_array":["1001","1002","1003","1004"],
+        "lower_limit":-55,
+        "upper_limit":50,
+        "step_test_threshold": 7.5,
+        "step_test_threshold_type": "absolute"
+        */
+        
+        QualityControlTest.printTestName();
+
+        Integer[] weatherParameters = {1001, 1002};
+        // below (-55.1) is incorrect, but equal to limit (-55.0) is correct
+        Double[][] data = {{0.0, 0.0}};
+
+        WeatherData testData = QualityControlTest.getWeatherDataForTests(weatherParameters, data);
+        Integer[] result = QualityControlTest.getQCResultForTests(testData, "RT");
+
+        Integer[] expResult = {2, 2};
+
+        assertArrayEquals(expResult,result);
+    }
+
+    @Test
+    public void testRTQCIntervalLowerLimit() throws Exception {
+        /*
+        "parameter":"Air temperature",
+        "id_array":["1001","1002","1003","1004"],
+        "lower_limit":-55,
+        "upper_limit":50,
+        "step_test_threshold": 7.5,
+        "step_test_threshold_type": "absolute"
+        */
+        
+        QualityControlTest.printTestName();
+
+        Integer[] weatherParameters = {1001, 1002};
+        // below (-55.1) is incorrect, but equal to limit (-55.0) is correct
+        Double[][] data = {{-55.1, -55.0}, {0.0, 0.0}};
+
+        WeatherData testData = QualityControlTest.getWeatherDataForTests(weatherParameters, data);
+        Integer[] result = QualityControlTest.getQCResultForTests(testData, "RT");
+
+        Integer[] expResult = {8, 2};
+
+        assertArrayEquals(expResult,result);
+    }
+
+    @Test
+    public void testRTQCIntervalUpperLimit() throws Exception {
+        /*
+        "parameter":"Air temperature",
+        "id_array":["1001","1002","1003","1004"],
+        "lower_limit":-55,
+        "upper_limit":50,
+        "step_test_threshold": 7.5,
+        "step_test_threshold_type": "absolute"
+        */
+
+        QualityControlTest.printTestName();
+
+        Integer[] weatherParameters = {1001, 1002};
+        // above (50.1) is incorrect, but equal to limit (50.0) is correct
+        Double[][] data = {{50.1, 50.0}, {0.0, 0.0}};
+
+        WeatherData testData = QualityControlTest.getWeatherDataForTests(weatherParameters, data);
+        Integer[] result = QualityControlTest.getQCResultForTests(testData, "RT");
+
+        Integer[] expResult = {8, 2};
+
+        assertArrayEquals(expResult,result);
+    }
+
+    // ------------------------
+    // RT - LOGICAL QC
+    // ------------------------
+
+    @Test
+    public void testRTQCLogicalSuccess() throws Exception {
+        QualityControlTest.printTestName();
+
+        Integer[] weatherParameters = {1003, 1002, 1004};
+        Double[][] data = {{0.0, 1.0, 2.0}, {0.0, 0.0, 0.0}};
+
+        WeatherData testData = QualityControlTest.getWeatherDataForTests(weatherParameters, data);
+        Integer[] result = QualityControlTest.getQCResultForTests(testData, "RT");
+
+        Integer[] expResult = {2, 2, 2};
+
+        assertArrayEquals(expResult,result);
+    }
+
+    @Test
+    public void testRTQCLogicalMinMaxIncorrect() throws Exception {
+        QualityControlTest.printTestName();
+
+        Integer[] weatherParameters = {1003, 1004};
+        // min > max
+        Double[][] data = {{1.0, 0.0}, {0.0, 0.0}};
+
+        WeatherData testData = QualityControlTest.getWeatherDataForTests(weatherParameters, data);
+        Integer[] result = QualityControlTest.getQCResultForTests(testData, "RT");
+
+        Integer[] expResult = {16, 16};
+
+        assertArrayEquals(expResult,result);
+    }
+
+    @Test
+    public void testRTQCLogicalMinMeanIncorrect() throws Exception {
+        QualityControlTest.printTestName();
+
+        Integer[] weatherParameters = {1003, 1002, 1004};
+        // min > mean
+        Double[][] data = {{1.0, 0.0, 2.0}, {0.0, 0.0, 0.0}};
+
+        WeatherData testData = QualityControlTest.getWeatherDataForTests(weatherParameters, data);
+        Integer[] result = QualityControlTest.getQCResultForTests(testData, "RT");
+
+        Integer[] expResult = {16, 16, 2};
+
+        assertArrayEquals(expResult,result);
+    }
+
+    @Test
+    public void testRTQCLogicalMeanMaxIncorrect() throws Exception {
+        QualityControlTest.printTestName();
+
+        Integer[] weatherParameters = {1003, 1002, 1004};
+        // mean > max
+        Double[][] data = {{0.0, 1.0, 0.0}, {0.0, 0.0, 0.0}};
+
+        WeatherData testData = QualityControlTest.getWeatherDataForTests(weatherParameters, data);
+        Integer[] result = QualityControlTest.getQCResultForTests(testData, "RT");
+
+        Integer[] expResult = {2, 16, 16};
+
+        assertArrayEquals(expResult,result);
+    }
+
+    // ------------------------
+    // NON RT - STEP QC
+    // ------------------------
+
+    @Test
+    public void testNoNRTQCStepSuccess() throws Exception {
+        /*
+        "parameter":"Air temperature",
+        "id_array":["1001","1002","1003","1004"],
+        "lower_limit":-55,
+        "upper_limit":50,
+        "step_test_threshold": 7.5,
+        "step_test_threshold_type": "absolute"
+        */
+
+        QualityControlTest.printTestName();
+
+        Integer[] weatherParameters = {1001, 1002, 1003};
+        // Sanity check: values increasing, decreasing or staying same doesn't 
+        // break things, when inside step threshold.
+        Double[][] data = {{0.0, 1.0, 0.0}, {1.0, 0.0, 0.0}};
+
+        WeatherData testData = QualityControlTest.getWeatherDataForTests(weatherParameters, data);
+        Integer[] result = QualityControlTest.getQCResultForTests(testData, "NONRT");
+
+        Integer[] expResult = {2, 2, 2};
+
+        assertArrayEquals(expResult,result);
+    }
+
+    @Test
+    public void testNoNRTQCStepPrecipitationIsNotTested() throws Exception {
+        /*
+        "parameter":"Precipitation",
+        "id_array":["2001"],
+        "lower_limit":-0.5,
+        "upper_limit":100
+        */
+        
+        QualityControlTest.printTestName();
+
+        Integer[] weatherParameters = {2001};
+        // no step tests are done for precipitation, even if it does maximum jumps
+        Double[][] data = {{-0.5}, {100.0}, {-0.5}, {100.0}};
+
+        WeatherData testData = QualityControlTest.getWeatherDataForTests(weatherParameters, data);
+        Integer[] result = QualityControlTest.getQCResultForTests(testData, "NONRT");
+
+        Integer[] expResult = {2};
+
+        assertArrayEquals(expResult,result);
+    }
+    
+    @Test
+    public void testNoNRTQCStepAbsolute() throws Exception {
+        /*
+        "parameter":"Air temperature",
+        "id_array":["1001","1002","1003","1004"],
+        "lower_limit":-55,
+        "upper_limit":50,
+        "step_test_threshold": 7.5,
+        "step_test_threshold_type": "absolute"
+        */
+        
+        QualityControlTest.printTestName();
+
+        Integer[] weatherParameters = {1001, 1002, 1003, 1004};
+        // 1001: large positive failing step
+        // 1002: large negative failing step
+        // 1003: minimal failing step
+        // 1004: maximal non-failing step
+        Double[][] data = {{-55.0, 50.0, 0.0, 0.0}, {50.0, -55.0, 7.6, 7.5}, {50.0, -55.0, 7.6, 7.5}};
+
+        WeatherData testData = QualityControlTest.getWeatherDataForTests(weatherParameters, data);
+        Integer[] result = QualityControlTest.getQCResultForTests(testData, "NONRT");
+
+        Integer[] expResult = {32, 32, 32, 2};
+
+        assertArrayEquals(expResult,result);
+    }
+
+    
+    @Test
+    public void testNoNRTQCStepRelative() throws Exception {
+        /*
+        "parameter":"Relative humidty",
+        "id_array":["3001","3002","3003","3004","3021","3022","3023","3024"],
+        "lower_limit":0,
+        "upper_limit":103,
+        "step_test_threshold": 30,
+        "step_test_threshold_type": "relative"
+        */
+        
+        QualityControlTest.printTestName();
+
+        Integer[] weatherParameters = {3001, 3002, 3021, 3031};
+        // 3001: large positive failing step
+        // 3002: large negative failing step
+        // 3021: minimal failing step
+        // 3022: maximal non-failing step
+        Double[][] data = {{0.0, 103.0, 100.0, 0.0}, {103.0, 0.0, 70.0, 30.0}, {103.0, 0.0, 70.0, 60.0}};
+
+        WeatherData testData = QualityControlTest.getWeatherDataForTests(weatherParameters, data);
+        Integer[] result = QualityControlTest.getQCResultForTests(testData, "NONRT");
+
+        Integer[] expResult = {32, 32, 32, 2};
+
+        assertArrayEquals(expResult,result);
+    }
+
+
+    // ------------------------
+    // NON RT - FREEZE QC
+    // ------------------------
+
+    @Test
+    public void testNoNRTQCFreezeSuccess() throws Exception {
+        /*
+        "parameter":"Relative humidty",
+        "id_array":["3001","3002","3003","3004","3021","3022","3023","3024"],
+        "lower_limit":0,
+        "upper_limit":103,
+        "step_test_threshold": 30,
+        "step_test_threshold_type": "relative"
+        */
+        
+        QualityControlTest.printTestName();
+
+        Integer[] weatherParameters = {1001, 1002, 1003};
+        // Sanity check: values changing doesn't break things.
+        // monotonically increasing
+        // monotonically decreasing 
+        // random jumps
+        Double[][] data = {
+            {0.0, 7.0, 10.0}, // 1st
+            {1.0, 6.0, 11.0}, // 2nd
+            {2.0, 5.0, 12.0}, // 3rd
+            {3.0, 4.0, 11.0}, // 4th
+            {4.0, 3.0, 10.0}, // 5th
+            {5.0, 2.0, 11.0}, // 6th
+            {6.0, 1.0, 12.0}, // 7th
+            {7.0, 0.0, 11.0}, // 8th
+        };
+
+        WeatherData testData = QualityControlTest.getWeatherDataForTests(weatherParameters, data);
+        Integer[] result = QualityControlTest.getQCResultForTests(testData, "NONRT");
+
+        Integer[] expResult = {2, 2, 2};
+
+        assertArrayEquals(expResult,result);
+    }
+
+    @Test
+    public void testNoNRTQCFreezePrecipitationZeroCannotFail() throws Exception {
+        /*
+        "parameter":"Precipitation",
+        "id_array":["2001"],
+        "lower_limit":-0.5,
+        "upper_limit":100
+        */
+        
+        QualityControlTest.printTestName();
+
+        Integer[] weatherParameters = {2001};
+        // Precipitation value 0.0 is a special case that cannot fail freeze QC.
+        Double[][] data = {
+            {0.0}, // 1st
+            {0.0}, // 2nd
+            {0.0}, // 3rd
+            {0.0}, // 4th
+            {0.0}, // 5th
+            {0.0}, // 6th
+            {0.0}, // 7th
+            {0.0}, // 8th
+        };
+
+        WeatherData testData = QualityControlTest.getWeatherDataForTests(weatherParameters, data);
+        Integer[] result = QualityControlTest.getQCResultForTests(testData, "NONRT");
+
+        Integer[] expResult = {2};
+
+        assertArrayEquals(expResult,result);
+    }
+    
+    @Test
+    public void testNoNRTQCFreezeFailsWithHourlyDataInterval() throws Exception {        
+        QualityControlTest.printTestName();
+
+        Integer[] weatherParameters = {1001, 1101, 3001, 3002};
+        // 1001: fail - all zeroes (only precipitation should allow 0.0 freezing).
+        // 1101: fail - all random number
+        // 3001: fail - minimal failing freeze
+        // 4001: success - maximal non-failing freeze
+        Double[][] data = {
+            {0.0, 60.0,  99.0,  99.0}, // 1st - 1001 and 1101 start freeze
+            {0.0, 60.0, 100.0, 100.0}, // 2nd - 3001 and 3002 start freeze
+            {0.0, 60.0, 100.0, 100.0}, // 3rd
+            {0.0, 60.0, 100.0, 100.0}, // 4th
+            {0.0, 60.0, 100.0, 100.0}, // 5th
+            {0.0, 60.0, 100.0, 100.0}, // 6th - 1001 and 1101 fail
+            {0.0, 60.0, 100.0, 100.0}, // 7th
+            {0.0, 60.0, 100.0,  99.0}, // 8th - 3001 fails
+        };
+
+        WeatherData testData = QualityControlTest.getWeatherDataForTests(weatherParameters, data);
+        Integer[] result = QualityControlTest.getQCResultForTests(testData, "NONRT");
+
+        Integer[] expResult = {64, 64, 64, 2};
+
+        assertArrayEquals(expResult,result);
+    }
+
+    @Test
+    public void testNoNRTQCFreezeFailsWithTwoHourDataInterval() throws Exception {
+        QualityControlTest.printTestName();
+
+        Integer[] weatherParameters = {1001, 1101, 3001, 3002};
+        // 1001: fail - all zeroes (only precipitation should allow 0.0 freezing).
+        // 1101: fail - all random number
+        // 3001: fail - minimal failing freeze
+        // 4001: success - maximal non-failing freeze
+        Double[][] data = {
+            {0.0, 60.0, 100.0, 100.0}, // 1st
+            {0.0, 60.0, 100.0, 100.0}, // 2nd
+            {0.0, 60.0, 100.0,  99.0}, // 3rd 1001, 1101 and 3001 fail
+            {0.0, 60.0,  99.0,  99.0}, // 4th
+        };
+
+        Integer twoHoursInSeconds = 7200;
+
+        WeatherData testData = QualityControlTest.getWeatherDataForTests(weatherParameters, data);
+        testData.setInterval(twoHoursInSeconds);
+        Integer[] result = QualityControlTest.getQCResultForTests(testData, "NONRT");
+
+        Integer[] expResult = {64, 64, 64, 2};
+
+        assertArrayEquals(expResult,result);
+    }
+}
