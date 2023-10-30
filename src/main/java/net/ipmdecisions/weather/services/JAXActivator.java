63b0a54a7aa775fc01e2d4a5c1ae458f03855c3b
Tor-Einar Skog <tor-einar.skog@nibio.no>
2023-10-30 10:37:28 +0100

    Fix: Remove NetBeans auto generated resources list

--- src/main/java/net/ipmdecisions/weather/services/JAXActivator.java
+++ src/main/java/net/ipmdecisions/weather/services/JAXActivator.java
@@ -34,7 +34,7 @@ public class JAXActivator extends Application{
      @Override
     public Set<Class<?>> getClasses() {
         Set<Class<?>> resources = new java.util.HashSet<>();
-        addRestResourceClasses(resources);
+        addRestResourceClassesManual(resources);
         return resources;
     }
     
@@ -47,8 +47,8 @@ public class JAXActivator extends Application{
         singletons.add(corsFilter);
         return singletons;
     }
-
-    private void addRestResourceClasses(Set<Class<?>> resources) {
+    
+    private void addRestResourceClassesManual(Set<Class<?>> resources) {
         resources.add(net.ipmdecisions.weather.services.AmalgamationService.class);
         resources.add(net.ipmdecisions.weather.services.JacksonConfig.class);
         resources.add(net.ipmdecisions.weather.services.MetaDataService.class);
@@ -56,5 +56,48 @@ public class JAXActivator extends Application{
         resources.add(net.ipmdecisions.weather.services.TestDataService.class);
         resources.add(net.ipmdecisions.weather.services.WeatherAdapterService.class);
         resources.add(net.ipmdecisions.weather.services.WeatherDataSourceService.class);
+    }
+
+    /**
+     * This method is being auto populated by NetBeans, which often blows up the application
+     * @param resources 
+     */
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(com.webcohesion.enunciate.rt.EnunciateJaxbContextResolver.class);
+        resources.add(net.ipmdecisions.weather.services.AmalgamationService.class);
+        resources.add(net.ipmdecisions.weather.services.JacksonConfig.class);
+        resources.add(net.ipmdecisions.weather.services.MetaDataService.class);
+        resources.add(net.ipmdecisions.weather.services.QualityControlService.class);
+        resources.add(net.ipmdecisions.weather.services.TestDataService.class);
+        resources.add(net.ipmdecisions.weather.services.WeatherAdapterService.class);
+        resources.add(net.ipmdecisions.weather.services.WeatherDataSourceService.class);
+        resources.add(org.jboss.resteasy.core.AcceptHeaderByFileSuffixFilter.class);
+        resources.add(org.jboss.resteasy.core.AsynchronousDispatcher.class);
+        resources.add(org.jboss.resteasy.plugins.interceptors.AcceptEncodingGZIPFilter.class);
+        resources.add(org.jboss.resteasy.plugins.interceptors.GZIPDecodingInterceptor.class);
+        resources.add(org.jboss.resteasy.plugins.interceptors.GZIPEncodingInterceptor.class);
+        resources.add(org.jboss.resteasy.plugins.interceptors.MessageSanitizerContainerResponseFilter.class);
+        resources.add(org.jboss.resteasy.plugins.providers.AsyncStreamingOutputProvider.class);
+        resources.add(org.jboss.resteasy.plugins.providers.ByteArrayProvider.class);
+        resources.add(org.jboss.resteasy.plugins.providers.DataSourceProvider.class);
+        resources.add(org.jboss.resteasy.plugins.providers.DefaultBooleanWriter.class);
+        resources.add(org.jboss.resteasy.plugins.providers.DefaultNumberWriter.class);
+        resources.add(org.jboss.resteasy.plugins.providers.DefaultTextPlain.class);
+        resources.add(org.jboss.resteasy.plugins.providers.DocumentProvider.class);
+        resources.add(org.jboss.resteasy.plugins.providers.FileProvider.class);
+        resources.add(org.jboss.resteasy.plugins.providers.FileRangeWriter.class);
+        resources.add(org.jboss.resteasy.plugins.providers.FormUrlEncodedProvider.class);
+        resources.add(org.jboss.resteasy.plugins.providers.IIOImageProvider.class);
+        resources.add(org.jboss.resteasy.plugins.providers.InputStreamProvider.class);
+        resources.add(org.jboss.resteasy.plugins.providers.JaxrsFormProvider.class);
+        resources.add(org.jboss.resteasy.plugins.providers.JaxrsServerFormUrlEncodedProvider.class);
+        resources.add(org.jboss.resteasy.plugins.providers.MultiValuedParamConverterProvider.class);
+        resources.add(org.jboss.resteasy.plugins.providers.ReaderProvider.class);
+        resources.add(org.jboss.resteasy.plugins.providers.SourceProvider.class);
+        resources.add(org.jboss.resteasy.plugins.providers.StreamingOutputProvider.class);
+        resources.add(org.jboss.resteasy.plugins.providers.StringTextStar.class);
+        resources.add(org.jboss.resteasy.plugins.providers.sse.SseEventProvider.class);
+        resources.add(org.jboss.resteasy.plugins.providers.sse.SseEventSinkInterceptor.class);
+
     }
 }
